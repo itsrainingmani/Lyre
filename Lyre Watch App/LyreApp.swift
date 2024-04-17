2b10cad4fc5efa9743b77fa34b7d45a8643d0e79
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-04-16 20:58:49 -0400

    Initial Commit

---
+++ Lyre Watch App/LyreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyreApp.swift
+//  Lyre Watch App
+//
+//  Created by Manikandan Sundararajan on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Lyre_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
